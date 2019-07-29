b217072678eae7e7d2e02c360771928143631fa8
Jaivardhan Mehrotra <jaivardhan_mehrotra@apple.com>
2019-07-29 02:27:30 -0700

    class inheritance test added

---
+++ class-inheritance-test.js
@@ -0,0 +1,53 @@
+class Shape {
+    constructor() {
+        this.name = 'shape';
+    }
+
+    area() {
+        return 0;
+    }
+
+    getName() {
+        return this.name;
+    }
+}
+
+class Circle extends Shape {
+    constructor(radius) {
+        super();
+        this.name = 'circle';
+        this.radius = radius;
+    }
+
+    area() {
+        return Math.PI * this.radius^2;
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(length, width) {
+        super();
+        this.name = 'rectangle';
+        this.length = length;
+        this.width = width;
+    }
+
+    area() {
+        return this.length * this.width;
+    }
+}
+
+class Square extends Rectangle {
+    constructor(size) {
+        super(size, size);
+        this.name = 'square';
+    }
+}
+
+const circle = new Circle(5);
+const rectangle = new Rectangle(10, 5);
+const square = new Square(6);
+
+console.log(`${circle.getName()}: ${circle.area()}`);
+console.log(`${rectangle.getName()}: ${rectangle.area()}`);
+console.log(`${square.getName()}: ${square.area()}`);
